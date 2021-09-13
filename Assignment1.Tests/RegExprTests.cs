93df25e4c993661d0957f1dc34e9123be0f16e5a
simonlk <simon@kristianzen.dk>
2021-09-13 17:02:28 +0200

    implement and test SplitLine + Resolutions

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,8 +1,88 @@
 using Xunit;
+using System;
+using System.Collections.Generic;
 
 namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void SplitLine_given_Hej_med_dig_returns_3_lines()
+        {
+            // Arrange
+            var input = new[] { "Hej", "med", "dig" };
+
+            // Act
+            var output = RegExpr.SplitLine(input);
+
+            // Assert
+            Assert.Equal(new[] { "Hej\n", "med\n", "dig\n" }, output);
+        }
+
+        [Fact]
+        public void SplitLine_given_Hej_med_dig_and_whitespace_returns_3_lines()
+        {
+            // Arrange
+            var input = new[] { "Hej    ", " med  ", "dig" };
+
+            // Act
+            var output = RegExpr.SplitLine(input);
+
+            // Assert
+            Assert.Equal(new[] { "Hej\n", "med\n", "dig\n" }, output);
+        }
+
+        [Fact]
+        public void SplitLine_given_Hej23_med1_dig_50_returns_4_lines()
+        {
+            // Arrange
+            var input = new[] { "Hej23", "med1", "dig", "50" };
+
+            // Act
+            var output = RegExpr.SplitLine(input);
+
+            // Assert
+            Assert.Equal(new[] { "Hej23\n", "med1\n", "dig\n", "50\n" }, output);
+        }
+
+        [Fact]
+        public void SplitLine_given_Hej23_med1_dig_50_and_whitespace_returns_4_lines()
+        {
+            // Arrange
+            var input = new[] { "  Hej23  ", " med1", "dig  ", "50" };
+
+            // Act
+            var output = RegExpr.SplitLine(input);
+
+            // Assert
+            Assert.Equal(new[] { "Hej23\n", "med1\n", "dig\n", "50\n" }, output);
+        }
+
+        [Fact]
+        public void SplitLine_given_Hej23_med1_dig_50_and_whitespace_mixed_string_returns_4_lines()
+        {
+            // Arrange
+            var input = new[] { "  Hej23  med1", " dig   50 " };
+
+            // Act
+            var output = RegExpr.SplitLine(input);
+
+            // Assert
+            Assert.Equal(new[] { "Hej23\n", "med1\n", "dig\n", "50\n" }, output);
+        }
+
+        [Fact]
+        public void Resolutions_given_1920x1080_1024x768_800x600_returns_3_tuples()
+        {
+            // Arrange
+            var input = "1920x1080 1024x768 800x600";
+
+            // Act
+            var output = RegExpr.Resolution(input);
+            var expected = new List<(int, int)> { (1920, 1080), (1024, 768), (800, 600) };
+
+            // Assert
+            Assert.Equal(output, expected);
+        }
     }
 }
--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
 
 namespace Assignment1
 {
@@ -7,12 +10,36 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            var pattern = @"[a-zA-Z0-9]+";
+            foreach (var line in lines)
+            {
+                var matches = Regex.Matches(line, pattern);
+
+                foreach (var match in matches)
+                {
+                    var stringBuilder = new StringBuilder(match.ToString());
+                    stringBuilder.Append("\n");
+                    var result = stringBuilder.ToString();
+                    yield return result;
+                }
+            }
         }
 
+        //perhaps we should have some helper methods for this method
+        //perhaps also consider error handling for integer parsing?
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
         {
-            throw new NotImplementedException();
+            var pattern = "(?<width>[0-9]+)x(?<height>[0-9]+)";
+            var matches = Regex.Matches(resolutions, pattern);
+
+            //why does var treat match as an object?
+            foreach (Match match in matches)
+            {
+                var width = Int32.Parse(match.Groups["width"].Value);
+                var height = Int32.Parse(match.Groups["height"].Value);
+
+                yield return (width, height);
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
