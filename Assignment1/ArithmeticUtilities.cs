b817f27576cb5d294b7b2735aae580b0faf8e5f8
simonlk <simon@kristianzen.dk>
2021-09-13 12:48:54 +0200

    implement + test flatten and filter

---
+++ Assignment1/ArithmeticUtilities.cs
@@ -0,0 +1,10 @@
+namespace Assignment1
+{
+    public static class ArithmeticUtilities
+    {
+        public static bool Even(int number)
+        {
+            return number % 2 == 0;
+        }
+    }
+}
